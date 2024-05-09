a14b718faff2e255d94822ea3d9e087599799114
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-05-09 22:29:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function changeBackground(){
+
+  const portrait = "https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg" ;
+  const landscape = "https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg";
+
+
+  if(window.innerWidth > window.innerHeight){
+
+    document.body.style.background =  `url(${landscape})`;
+  }else{
+    document.body.style.background = `url(${portrait})`;
+  }
+
+
+}
+
+changeBackground();
+
+
+window.addEventListener("resize",changeBackground)
